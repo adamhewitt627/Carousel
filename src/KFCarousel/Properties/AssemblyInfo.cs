b926fe6c644761e0858fa1a33bcf825eb0847877
Adam Hewitt <adamhewitt@olivetree.com>
2019-11-15 07:45:48 -0800

    Rename project and a working NUSPEC

--- src/KFCarousel/Properties/AssemblyInfo.cs
+++ src/KFCarousel/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CarouselRepeater")]
+[assembly: AssemblyTitle("KFCarousel")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CarouselRepeater")]
+[assembly: AssemblyProduct("KFCarousel")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]

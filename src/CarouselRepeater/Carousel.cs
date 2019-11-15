ae709daad1ca002e4805ae6fdf0f541a199a01a6
Adam Hewitt <adamhewitt@olivetree.com>
2019-11-14 21:07:34 -0800

    Initial project creation

---
+++ src/CarouselRepeater/Carousel.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace KF.CarouselRepeater
+{
+    public sealed class Carousel : Control
+    {
+        public Carousel()
+        {
+            DefaultStyleKey = typeof(Carousel);
+        }
+    }
+}
---
+++ src/CarouselRepeater/CarouselRepeater.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace KF.CarouselRepeater
+{
+    public sealed class CarouselRepeater : ItemsRepeater
+    {
+    }
+}

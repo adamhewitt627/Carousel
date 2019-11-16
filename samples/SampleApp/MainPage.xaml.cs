a468cef9e5f6d92d3b67da122688c72031527811
Adam Hewitt <adamhewitt627@users.noreply.github.com>
2019-11-15 17:39:55 -0800

    Virtualized Layout (#2)
    * Cleanup layout code as a non-virtualized
    
    * A little cleanup of responsibilities
    
    * Eh, I don't know anymore
    
    * This is working better, rely on UniformGridLayout over a custom layout
    
    * Finally, StackLayout and snap points allow for a virtualized (varying size) carousel

--- samples/SampleApp/MainPage.xaml.cs
+++ samples/SampleApp/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using Windows.UI.Xaml.Controls;
 
 namespace SampleApp
 {
@@ -7,7 +9,12 @@ namespace SampleApp
         public MainPage()
         {
             InitializeComponent();
-            carousel.ItemsSource = new[] { "First", "Second", "Third", "Fourth", "Fifth", "Sixth", "Seventh", "Eighth" };
+
+            //var source = new ObservableCollection<string>(new[] { "First", "Second", "Third", "Fourth", "Fifth", "Sixth", "Seventh" });
+            var source = new ObservableCollection<int>(Enumerable.Range(1, 17));
+            carousel.ItemsSource = source;
+
+            Loaded += (o, e) => source.Add(source.Count + 1);
         }
     }
 }
--- src/KFCarousel/CarouselRepeater.cs
+++ src/KFCarousel/CarouselRepeater.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.UI.Xaml.Controls;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Windows.Foundation;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -8,109 +9,70 @@ using Windows.UI.Xaml.Controls.Primitives;
 
 namespace KFCarousel
 {
-    public sealed class CarouselRepeater : ItemsRepeater, IScrollSnapPointsInfo
+    internal class CarouselRepeater : ItemsRepeater, IScrollSnapPointsInfo
     {
         public CarouselRepeater()
         {
-            Layout = new CarouselLayout();
+            Layout = new StackLayout
+            {
+                Orientation = Orientation.Horizontal,
+            };
+
             EffectiveViewportChanged += CarouselRepeater_EffectiveViewportChanged;
+            ElementPrepared += CarouselRepeater_ElementPrepared;
+        }
+
+        private static readonly DependencyProperty ViewportProperty =
+            DependencyProperty.Register(nameof(Viewport), typeof(Rect), typeof(CarouselRepeater), new PropertyMetadata(Rect.Empty));
+        private Rect Viewport
+        {
+            get { return (Rect)GetValue(ViewportProperty); }
+            set { SetValue(ViewportProperty, value); }
         }
 
         private void CarouselRepeater_EffectiveViewportChanged(FrameworkElement sender, EffectiveViewportChangedEventArgs args)
         {
-            if (Layout is CarouselLayout layout)
-                layout.Viewport = args.MaxViewport;
+            Viewport = args.MaxViewport;
+            HorizontalSnapPointsChanged?.Invoke(this, EventArgs.Empty);
         }
+
+        private void CarouselRepeater_ElementPrepared(ItemsRepeater sender, ItemsRepeaterElementPreparedEventArgs args) 
+            => HorizontalSnapPointsChanged?.Invoke(this, EventArgs.Empty);
 
         #region IScrollSnapPointsInfo
 
-        public IReadOnlyList<float> GetIrregularSnapPoints(Orientation orientation, SnapPointsAlignment alignment) => new List<float>();
-
-        public float GetRegularSnapPoints(Orientation orientation, SnapPointsAlignment alignment, out float offset)
+        public IReadOnlyList<float> GetIrregularSnapPoints(Orientation orientation, SnapPointsAlignment alignment)
         {
-            offset = 0;
-            if (orientation == Orientation.Vertical)
-                return 0f;
+            return alignment switch
+            {
+                SnapPointsAlignment.Center => center().OrderBy(x => x).ToList(),
+                _ => new List<float>(), //TODO
+            };
 
-            var viewport = ((CarouselLayout)Layout).Viewport;
-            offset = (float)Math.Round(viewport.Width * 0.5);
-            return (float)Math.Round(viewport.Width * 0.9);
+            IEnumerable<float> center()
+            {
+                var center = (float)Viewport.Width / 2;
+                yield return center;
+
+                for (var i = 0; i < ItemsSourceView.Count; i++)
+                {
+                    if (TryGetElement(i) is UIElement e)
+                        yield return e.ActualOffset.X + (e.ActualSize.X / 2);
+                }
+
+                yield return ActualSize.X - center;
+            }
         }
 
-        public bool AreHorizontalSnapPointsRegular => true;
-        public bool AreVerticalSnapPointsRegular => true;
+        public float GetRegularSnapPoints(Orientation orientation, SnapPointsAlignment alignment, out float offset) => offset = 0f;
+        public bool AreHorizontalSnapPointsRegular => false;
+        public bool AreVerticalSnapPointsRegular => false;
 
-#pragma warning disable CS0067  //The event is never used
-        public event EventHandler<object> HorizontalSnapPointsChanged;  
+#pragma warning disable CS0067
+        public event EventHandler<object> HorizontalSnapPointsChanged;
         public event EventHandler<object> VerticalSnapPointsChanged;
 #pragma warning restore CS0067
 
         #endregion IScrollSnapPointsInfo
-
-        private class CarouselLayout : VirtualizingLayout
-        {
-            public static readonly DependencyProperty ViewportProperty = DependencyProperty.Register(nameof(Viewport), typeof(Rect), typeof(CarouselLayout),
-                new PropertyMetadata(Rect.Empty, (o, e) =>
-                {
-                    var viewport = (Rect)e.NewValue;
-                    o.SetValue(ItemWidthProperty, Math.Round(viewport.Width * 0.9));
-                }));
-            public Rect Viewport
-            {
-                get => (Rect)GetValue(ViewportProperty);
-                set => SetValue(ViewportProperty, value);
-            }
-
-            private static readonly DependencyProperty ItemWidthProperty = DependencyProperty.Register(nameof(ItemWidth), typeof(double), typeof(CarouselLayout),
-                new PropertyMetadata(double.PositiveInfinity, (o, e) => ((CarouselLayout)o).InvalidateMeasure()));
-            private double ItemWidth
-            {
-                get => (double)GetValue(ItemWidthProperty);
-                set => SetValue(ItemWidthProperty, value);
-            }
-
-            protected override Size MeasureOverride(VirtualizingLayoutContext context, Size availableSize)
-            {
-                if (double.IsInfinity(ItemWidth))
-                    return new Size(0, 0);
-
-                /*TODO:
-                 *  - Virtualizing
-                 *  - Wrapping (it's a layout, so why not?)
-                 */
-                var itemSize = new Size(ItemWidth, availableSize.Height);
-
-                for (int i = 0; i < context.ItemCount; i++)
-                {
-                    var element = context.GetOrCreateElementAt(i);
-                    element.Measure(itemSize);
-                }
-
-                var desiredSize = new Size(
-                    width: context.ItemCount * ItemWidth + Math.Round(Viewport.Width * .05) * 2,
-                    height: availableSize.Height
-                );
-                return desiredSize;
-            }
-
-            protected override Size ArrangeOverride(VirtualizingLayoutContext context, Size finalSize)
-            {
-                if (double.IsInfinity(ItemWidth))
-                    return finalSize;
-
-                var gutter = Math.Round(Viewport.Width * .05);
-                
-                var x = gutter;
-                for (int i = 0; i < context.ItemCount; i++)
-                {
-                    var element = context.GetOrCreateElementAt(i);
-                    element.Arrange(new Rect(x, 0, ItemWidth, finalSize.Height));
-                    x += ItemWidth;
-                }
-                x += gutter;
-
-                return new Size(x, finalSize.Height);
-            }
-        }
     }
 }
